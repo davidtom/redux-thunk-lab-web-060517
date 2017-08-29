3dcfbab1b547fcc1e5f59ec9dc64ac8e50c78b40
davidtom <davidjtomczyk@gmail.com>
2017-08-29 14:52:00 -0400

    fix store

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const store = createStore(rootReducer,
 
 ReactDOM.render(
     <Provider store={store}>
-      <WrapperApp store={store}/>
+      <WrapperApp/>
     </Provider>,
     document.getElementById('root')
 );
